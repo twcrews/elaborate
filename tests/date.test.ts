769d5f6b42a9b3399cd933011135adcf87524bf3
Tommy Crews <twcrews96@gmail.com>
2022-03-14 20:12:45 -0500

    Added friendlyTimespanString.

--- tests/date.test.ts
+++ tests/date.test.ts
@@ -1,4 +1,8 @@
-import { timespan, friendlyTimespanArray } from "../src/date";
+import {
+	timespan,
+	friendlyTimespanArray,
+	friendlyTimespanString,
+} from "../src/date";
 
 test("timespan - returns millisecond difference", () => {
 	const start = new Date(2020, 1, 1, 0, 0, 0, 0);
@@ -21,4 +25,15 @@ test("friendlyTimespanArray - converts to array of friendly units", () => {
 		"12 seconds",
 		"500 milliseconds",
 	]);
+});
+
+test("friendlyTimespanString - converts timespan to expected string", () => {
+	const start = new Date(2021, 1, 1, 0, 0, 0, 0);
+	const end = new Date(2022, 1, 8, 9, 30, 12, 500);
+
+	const ts = timespan(start, end);
+
+	expect(friendlyTimespanString(ts)).toBe(
+		"1 year, 1 week, 1 day, 9 hours, 30 minutes, 12 seconds, and 500 milliseconds"
+	);
 });
